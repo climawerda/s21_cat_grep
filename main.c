a3a214f534449d8a20e712a8cb94319b074e18fc
climawerda <119702267+climawerda@users.noreply.github.com>
2023-01-24 14:15:11 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "options.h"
+
+#include "s21_cat.h"
 #define MAXLINE 1000
 
 int main(int argc, char* argv[]) {
-    int i = 1;
-    if (argc == 2) {
-        nofile();
-    } else if (argv[2][0] == '-') {
-        for (int i = 3; i < argc; i++) {
-            optional(argv[2], argv[i]);
-        }
-    } else {
-        for (int i = 2; i < argc; i++) {
-            nooption(argv[i]);
-        }
+  int i = 1;
+  if (argc == 1) {
+    nofile();
+  } else if (argv[1][0] == '-') {
+    for (int i = 2; i < argc; i++) {
+      optional(argv[1], argv[i]);
     }
-    return 0;
+  } else {
+    for (int i = 1; i < argc; i++) {
+      nooption(argv[i]);
+    }
+  }
+  return 0;
 }
+
 
