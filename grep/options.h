e1d857a29ead58fadbd61814eba3bf35949f9db5
climawerda <119702267+climawerda@users.noreply.github.com>
2023-01-24 14:20:51 +0300

    Update and rename options.h to s21_grep.h

--- grep/options.h
+++
@@ -1,7 +0,0 @@
-#ifndef OPTIONS_H
-#define OPTIONS_H
-
-int optional(char* option, char** filename);
-int nooption(char** filename);
-
-#endif
---
+++ grep/s21_grep.h
@@ -0,0 +1,6 @@
+#ifndef S21_GREP_H
+#define S21_GREP_H
+
+void options(char *pat, char *filename, char opt, int flag);
+
+#endif
